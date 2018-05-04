f9b2a6c15656c54a37b26aa7095f8ba4d9ce9c22
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-05-04 14:17:49 -0400

    skeleton

---
+++ src/main/java/org/boudnik/ariadne/Resource.java
@@ -0,0 +1,8 @@
+package org.boudnik.ariadne;
+
+/**
+ * @author Alexandre_Boudnik
+ * @since 05/04/2018
+ */
+public interface Resource {
+}
---
+++ src/test/java/org/boudnik/ariadne/ResourceTest.java
@@ -0,0 +1,27 @@
+package org.boudnik.ariadne;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author Alexandre_Boudnik
+ * @since 05/04/2018
+ */
+public class ResourceTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void test() {
+        System.out.println("ResourceTest.test");
+    }
+
+    @After
+    public void tearDown() {
+    }
+}
