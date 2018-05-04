accb4a40fa0cda4dbffbf413942c5847d2246c01
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-05-04 15:13:35 -0400

    mocks & travis

--- src/main/java/org/boudnik/ariadne/Resource.java
+++ src/main/java/org/boudnik/ariadne/Resource.java
@@ -1,8 +1,31 @@
 package org.boudnik.ariadne;
 
+import java.util.Set;
+
 /**
  * @author Alexandre_Boudnik
  * @since 05/04/2018
  */
 public interface Resource {
+
+    String type();
+
+    /**
+     * Supposed to build this
+     */
+    default void build() {
+
+    }
+
+    Set<Resource> prerequisites();
+
+    boolean isReady();
+
+    default boolean isSatisfied() {
+        boolean ready = true;
+        for (Resource resource : prerequisites()) {
+            ready &= resource.isReady();
+        }
+        return ready;
+    }
 }
--- src/test/java/org/boudnik/ariadne/ResourceTest.java
+++ src/test/java/org/boudnik/ariadne/ResourceTest.java
@@ -3,22 +3,63 @@ package org.boudnik.ariadne;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 /**
  * @author Alexandre_Boudnik
  * @since 05/04/2018
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ResourceTest {
+    @Mock
+    private Resource resource;
+    @Mock
+    private Resource prerequisite;
 
     @Before
     public void setUp() {
+        assertNotNull(resource);
+        when(resource.prerequisites()).thenReturn(new HashSet<>(Collections.singletonList(prerequisite)));
+        when(resource.type()).thenReturn("Test");
+        when(resource.isSatisfied()).thenCallRealMethod();
+
+        when(prerequisite.isReady()).thenReturn(true);
     }
 
     @Test
-    public void test() {
-        System.out.println("ResourceTest.test");
+    public void type() {
+        assertSame("Test", resource.type());
+    }
+
+    @Test
+    public void build() {
+    }
+
+    @Test
+    public void prerequisites() {
+        Set<Resource> prerequisites = resource.prerequisites();
+        assertFalse(prerequisites.isEmpty());
+        assertTrue(prerequisites.contains(prerequisite));
+    }
+
+    @Test
+    public void isReady() {
+        assertTrue(prerequisite.isReady());
+        assertFalse(resource.isReady());
+    }
+
+    @Test
+    public void isSatisfied() {
+        assertTrue(resource.isSatisfied());
     }
 
     @After
