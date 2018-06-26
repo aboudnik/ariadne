c61c7b8b1bfc7ed1596f9ff879b8df2f102b8db1
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-06-26 13:24:28 -0400

    ariadne-25 Fork Join - add complex test with execution on grid

--- src/test/java/org/boudnik/ariadne/ForkJoinTest.java
+++ src/test/java/org/boudnik/ariadne/ForkJoinTest.java
@@ -1,9 +1,13 @@
 package org.boudnik.ariadne;
 
+import org.apache.ignite.Ignite;
+import org.apache.ignite.Ignition;
+import org.apache.ignite.lang.IgniteClosure;
 import org.junit.Test;
 
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.RecursiveTask;
+import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
 
@@ -41,39 +45,67 @@ public class ForkJoinTest {
 
             );
 
+    private IgniteClosure<Node, String> builder = node -> {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ignored) {
+        }
+        return node.n;
+    };
+
     @Test
     public void recursive() {
+        run(() -> new Tree(builder, root));
+    }
+
+    @Test
+    public void recursiveOnIgnite() {
+        try (Ignite ignite = Ignition.start()) {
+            run(() -> new Tree(node -> ignite.compute().apply(builder, node), root));
+        }
+    }
+
+    private void run(Supplier<RecursiveTask<String>> supplier) {
         String expected = " 1.1.1 1.1.2 1.1 1.2.1 1.2.2 1.2.3 1.2.4 1.2 1.3 1";
         for (int i : new int[]{1, 2, 4, 8}) {
             sw = new Stopwatch();
-            String actual = new ForkJoinPool(i).invoke(new Task(root));
+            String actual = new ForkJoinPool(i).invoke(supplier.get());
             System.out.printf("%d thread(s) %d secs %s%n", i, sw.seconds(), actual);
             assertEquals(expected, actual);
         }
     }
 
-    static class Task extends RecursiveTask<String> {
-        private final Node node;
+    static class Tree extends RecursiveTask<String> {
 
-        Task(Node node) {
+        private final Node node;
+        private final IgniteClosure<Node, String> function;
+
+        Tree(IgniteClosure<Node, String> function, Node node) {
             this.node = node;
+            this.function = function;
         }
 
         @Override
         protected String compute() {
-            System.out.println(Thread.currentThread() + " " + sw.seconds());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException ignored) {
-            }
-            Task[] tasks = new Task[node.children.length];
+            System.out.printf("%s %2d -> %s%n", Thread.currentThread(), sw.seconds(), node.n);
+            Tree[] tasks = new Tree[node.children.length];
+
+            // initiate dependencies calculation
             for (int i = 0; i < node.children.length; i++)
-                (tasks[i] = new Task(node.children[i])).fork();
+                (tasks[i] = new Tree(function, node.children[i])).fork();
+
+            // collect results from dependencies
             StringBuilder s = new StringBuilder();
-            for (Task task : tasks)
+            for (Tree task : tasks)
                 s.append(task.join());
-            return s + " " + node.n;
+
+            // and apply the function
+            s.append(" ").append(function.apply(node));
+
+            System.out.printf("%s %2d <- %s%n", Thread.currentThread(), sw.seconds(), node.n);
+            return s.toString();
         }
+
     }
 
     static class Stopwatch {
