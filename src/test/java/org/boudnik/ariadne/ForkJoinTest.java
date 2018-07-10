bea415453a657158083ca4cc0fc79fbbda8d7dbf
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-07-10 17:38:18 -0400

    ariadne-25 Fork Join - lambdas and f/j driver

--- src/test/java/org/boudnik/ariadne/ForkJoinTest.java
+++ src/test/java/org/boudnik/ariadne/ForkJoinTest.java
@@ -2,6 +2,7 @@ package org.boudnik.ariadne;
 
 import org.apache.ignite.Ignite;
 import org.apache.ignite.Ignition;
+import org.apache.ignite.lang.IgniteBiClosure;
 import org.apache.ignite.lang.IgniteClosure;
 import org.junit.Test;
 
@@ -10,6 +11,7 @@ import java.util.concurrent.RecursiveTask;
 import java.util.function.Supplier;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 /**
  * @author Alexandre_Boudnik
@@ -17,98 +19,86 @@ import static org.junit.Assert.assertEquals;
  */
 public class ForkJoinTest {
 
-    private static Stopwatch sw;
+    private static final int MILLIS = 200;
 
-    static class Node {
-        final String n;
-        final Node[] children;
+    static class TreeNode implements TreeTask.Node<String> {
+        private final String n;
+        private final TreeNode[] children;
 
-        Node(String n, Node... children) {
+        @Override
+        public TreeNode[] getChildren() {
+            return children;
+        }
+
+        @Override
+        public String getN() {
+            return n;
+        }
+
+        TreeNode(String n, TreeNode... children) {
             this.n = n;
             this.children = children;
         }
     }
 
-    private Node root =
-            new Node("1",
-                    new Node("1.1",
-                            new Node("1.1.1"),
-                            new Node("1.1.2")
+//    private static Stopwatch sw;
+
+    private TreeNode root =
+            new TreeNode("1",
+                    new TreeNode("1.1",
+                            new TreeNode("1.1.1"),
+                            new TreeNode("1.1.2")
                     ),
-                    new Node("1.2",
-                            new Node("1.2.1"),
-                            new Node("1.2.2"),
-                            new Node("1.2.3"),
-                            new Node("1.2.4")
+                    new TreeNode("1.2",
+                            new TreeNode("1.2.1"),
+                            new TreeNode("1.2.2"),
+                            new TreeNode("1.2.3"),
+                            new TreeNode("1.2.4")
                     ),
-                    new Node("1.3")
+                    new TreeNode("1.3")
 
             );
 
-    private IgniteClosure<Node, String> builder = node -> {
+    private IgniteClosure<TreeTask.Node<String>, String> builder = node -> {
         try {
-            Thread.sleep(1000);
+            Thread.sleep(MILLIS);
         } catch (InterruptedException ignored) {
         }
-        return node.n;
+        return node.getN();
     };
+
+    private IgniteBiClosure<String, String, String> combiner = (s1, s2) -> (s1 == null ? "" : (s1 + " ")) + s2;
+
 
     @Test
     public void recursive() {
-        run(() -> new Tree(builder, root));
+        run(() -> new TreeTask<>(root, builder, combiner));
+    }
+
+    @Test
+    public void noCombiner() {
+        assertNull(new ForkJoinPool(1).invoke(new TreeTask<>(root, builder, (s1, s2) -> null)));
     }
 
     @Test
     public void recursiveOnIgnite() {
         try (Ignite ignite = Ignition.start()) {
-            run(() -> new Tree(node -> ignite.compute().apply(builder, node), root));
+            run(() -> new TreeTask<>(root, node -> ignite.compute().apply(builder, node), combiner));
         }
     }
 
     private void run(Supplier<RecursiveTask<String>> supplier) {
-        String expected = " 1.1.1 1.1.2 1.1 1.2.1 1.2.2 1.2.3 1.2.4 1.2 1.3 1";
+        String expected = "1.1.1 1.1.2 1.1 1.2.1 1.2.2 1.2.3 1.2.4 1.2 1.3 1";
         for (int i : new int[]{1, 2, 4, 8}) {
-            sw = new Stopwatch();
+            Stopwatch sw = new Stopwatch();
             String actual = new ForkJoinPool(i).invoke(supplier.get());
             System.out.printf("%d thread(s) %d secs %s%n", i, sw.seconds(), actual);
             assertEquals(expected, actual);
         }
     }
 
-    static class Tree extends RecursiveTask<String> {
-
-        private final Node node;
-        private final IgniteClosure<Node, String> function;
-
-        Tree(IgniteClosure<Node, String> function, Node node) {
-            this.node = node;
-            this.function = function;
-        }
-
-        @Override
-        protected String compute() {
-            System.out.printf("%s %2d -> %s%n", Thread.currentThread(), sw.seconds(), node.n);
-            Tree[] tasks = new Tree[node.children.length];
-
-            // initiate dependencies calculation
-            for (int i = 0; i < node.children.length; i++)
-                (tasks[i] = new Tree(function, node.children[i])).fork();
-
-            // collect results from dependencies
-            StringBuilder s = new StringBuilder();
-            for (Tree task : tasks)
-                s.append(task.join());
-
-            // and apply the function
-            s.append(" ").append(function.apply(node));
-
-            System.out.printf("%s %2d <- %s%n", Thread.currentThread(), sw.seconds(), node.n);
-            return s.toString();
-        }
-
-    }
-
     static class Stopwatch {
+
         long start = System.currentTimeMillis();
 
         long mills() {
@@ -116,7 +106,7 @@ public class ForkJoinTest {
         }
 
         long seconds() {
-            return mills() / 1000;
+            return mills() / MILLIS;
         }
 
     }
