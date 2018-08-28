1b9cd0f8842f552a19a49d63ddf34f07b2b558f6
Alexandre Boudnik <Alexandre_Boudnik@epam.com>
2018-08-27 20:05:36 -0400

    to check how MVEL templates are working

---
+++ src/test/java/org/apache/ariadne/template/TemplateTest.java
@@ -0,0 +1,53 @@
+package org.apache.ariadne.template;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mvel2.templates.CompiledTemplate;
+import org.mvel2.templates.TemplateCompiler;
+import org.mvel2.templates.TemplateRuntime;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author Alexandre_Boudnik
+ * @since 08/27/2018
+ */
+public class TemplateTest {
+
+    private final List<Instrument> instruments = new ArrayList<Instrument>() {{
+        add(new Instrument("US11CM1LB"));
+        add(new Instrument("US10CM3AA"));
+        add(new Instrument("US11CM3AA"));
+        add(new Instrument("US21CM3AA"));
+        add(new Instrument("US31CM1AA"));
+        add(new Instrument("US40CM1AA"));
+        add(new Instrument("US22CM3AA"));
+        add(new Instrument("US67CM1AA"));
+        add(new Instrument("US73CM3AA"));
+        add(new Instrument("US74CM1AA"));
+    }};
+
+
+    public static class Instrument {
+        public String symbol;
+
+        Instrument(String symbol) {
+            this.symbol = symbol;
+        }
+    }
+
+    @Before
+    public void setup() {
+    }
+
+    @Test
+    public void main() {
+        CompiledTemplate template = TemplateCompiler.compileTemplate(TemplateTest.class.getResourceAsStream("/template.txt"));
+        String execute = (String) TemplateRuntime.execute(template, new HashMap<String, Object>() {{
+            put("instruments", instruments);
+        }});
+        System.out.println(execute);
+    }
+}
